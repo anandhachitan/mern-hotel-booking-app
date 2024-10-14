d8fd027abfa593c4433a28f87f921ff559253616
ANANDHACHITAN <anandhachitan03@gmail.com>
2024-10-14 20:25:43 +0530

    final part3

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -3,10 +3,12 @@ import { useParams } from "react-router-dom";
 import * as apiClient from "../api-client";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
 import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 const EditHotel = () => {
   const { hotelId } = useParams();
   const { showToast } = useAppContext();
+  const navigate = useNavigate();
   const { data: hotel } = useQuery(
     "fetchMyHotelById",
     () => apiClient.fetchMyHotelById(hotelId || ""),
@@ -18,6 +20,7 @@ const EditHotel = () => {
   const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
     onSuccess: () => {
       showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+      navigate("/my-hotels");
     },
     onError: () => {
       showToast({ message: "Error Saving Hotel", type: "ERROR" });
